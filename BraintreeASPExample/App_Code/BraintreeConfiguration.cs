d79ed9cd6b481dfe011fc60f9f7e4cb77c74f387
braintreeps <code@getbraintree.com>
2016-01-12 09:42:56 -0800

    Add BraintreeConfiguration class and configuration options in Web.config

---
+++ BraintreeASPExample/App_Code/BraintreeConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using Braintree;
+using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+
+namespace BraintreeASPExample
+{
+    public class BraintreeConfiguration
+    {
+        public Braintree.Environment Environment { get; set; }
+        public string MerchantId { get; set; }
+        public string PublicKey { get; set; }
+        public string PrivateKey { get; set; }
+        public BraintreeGateway gateway { get; private set; }
+
+        public BraintreeConfiguration() {
+            createGateway();
+        }
+
+        private void createGateway() {
+            Environment = getEnvironment();
+            MerchantId = getConfigurationSetting("BraintreeMerchantId");
+            PublicKey = getConfigurationSetting("BraintreePublicKey");
+            PrivateKey = getConfigurationSetting("BraintreePrivateKey");
+
+            gateway = new BraintreeGateway
+            {
+                Environment = Environment,
+                MerchantId = MerchantId,
+                PublicKey = PublicKey,
+                PrivateKey = PrivateKey
+            };
+        }
+
+        private String getConfigurationSetting(String setting)
+        {
+            return ConfigurationManager.AppSettings[setting];
+        }
+
+        private Braintree.Environment getEnvironment()
+        {
+            String environment = getConfigurationSetting("BraintreeEnvironment");
+            return environment == "production" ? Braintree.Environment.PRODUCTION : Braintree.Environment.SANDBOX;
+        }
+    }
+}
---
+++ BraintreeASPExampleTests/BraintreeConfigurationTest.cs
@@ -0,0 +1,22 @@
+﻿using Braintree;
+using BraintreeASPExample;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BraintreeASPExampleTests
+{
+    [TestClass]
+    public class BraintreeConfigurationTest
+    {
+        [TestMethod]
+        public void TestConfiguringGateway()
+        {
+            BraintreeConfiguration config = new BraintreeConfiguration();
+
+            Assert.AreEqual(config.gateway.Environment, Braintree.Environment.SANDBOX);
+            Assert.AreEqual(config.gateway.MerchantId, "MerchantId");
+            Assert.AreEqual(config.gateway.PublicKey, "PublicKey");
+            Assert.AreEqual(config.gateway.PrivateKey, "PrivateKey");
+        }
+    }
+}
