70e79737f9721355a916399f85ed5167c91a99a9
braintreeps <code@getbraintree.com>
2016-01-13 08:20:37 -0800

    Add BraintreeConfiguration interface

---
+++ BraintreeASPExample/App_Code/IBraintreeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Braintree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BraintreeASPExample
+{
+    public interface IBraintreeConfiguration
+    {
+        IBraintreeGateway createGateway();
+        String getConfigurationSetting(String setting);
+        Braintree.Environment getEnvironment();
+        IBraintreeGateway getGateway();
+    }
+}
