444d6060cb0c056a7a12bd11e15527983cbcf1c4
Boneva <iboneva@paypalcorp.com>
2016-04-04 07:56:08 -0700

    Style aspnet repo

--- BraintreeASPExample/Controllers/CheckoutsController.cs
+++ BraintreeASPExample/Controllers/CheckoutsController.cs
@@ -11,6 +11,16 @@ namespace BraintreeASPExample.Controllers
     {
         public IBraintreeConfiguration config = new BraintreeConfiguration();
 
+        public static readonly TransactionStatus[] transactionSuccessStatuses = {
+                                                                                    TransactionStatus.AUTHORIZED,
+                                                                                    TransactionStatus.AUTHORIZING,
+                                                                                    TransactionStatus.SETTLED,
+                                                                                    TransactionStatus.SETTLING,
+                                                                                    TransactionStatus.SETTLEMENT_CONFIRMED,
+                                                                                    TransactionStatus.SETTLEMENT_PENDING,
+                                                                                    TransactionStatus.SUBMITTED_FOR_SETTLEMENT
+                                                                                };
+
         public ActionResult New()
         {
             var gateway = config.GetGateway();
@@ -69,8 +79,23 @@ namespace BraintreeASPExample.Controllers
 
         public ActionResult Show(String id)
         {
+            string header, icon, message;
             var gateway = config.GetGateway();
             Transaction transaction = gateway.Transaction.Find(id);
+
+            if (transactionSuccessStatuses.Contains(transaction.Status))
+            {
+                TempData["header"] = "Sweet Success!";
+                TempData["icon"] = "success";
+                TempData["message"] = "Your test transaction has been successfully processed. See the Braintree API response and try again.";
+            }
+            else
+            {
+                 TempData["header"] = "Transaction Failed";
+                 TempData["icon"] = "fail";
+                 TempData["message"] = "Your test transaction has a status of " + transaction.Status + ". See the Braintree API response and try again.";
+             };
+
             ViewBag.Transaction = transaction;
             return View();
         }
