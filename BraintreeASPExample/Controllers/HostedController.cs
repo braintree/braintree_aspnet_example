433a3e84a93fcfb31235b466441e6d6c4558cdd2
Carl Sargunar <carl@sargunar.com>
2021-11-24 01:35:38 +0000

    added show controller

--- BraintreeASPExample/Controllers/HostedController.cs
+++ BraintreeASPExample/Controllers/HostedController.cs
@@ -29,9 +29,12 @@ namespace BraintreeASPExample.Controllers
             return View();
         }
 
-        public ActionResult Process(string nonce, string email, decimal amount)
+        public ActionResult Process()
         {
             var gateway = config.GetGateway();
+
+            var amount = Request["payment_amount"];
+            var nonce = Request["payment_method_nonce"];
 
             var request = new TransactionRequest
             {
@@ -44,9 +47,25 @@ namespace BraintreeASPExample.Controllers
             };
 
             var result = gateway.Transaction.Sale(request);
-
-            Transaction transaction = result.Target;
-            return RedirectToAction("Show", new { id = transaction.Id });
+            if (result.IsSuccess())
+            {
+                Transaction transaction = result.Target;
+                return RedirectToAction("Show", new { id = transaction.Id });
+            }
+            else if (result.Transaction != null)
+            {
+                return RedirectToAction("Show", new { id = result.Transaction.Id });
+            }
+            else
+            {
+                string errorMessages = "";
+                foreach (ValidationError error in result.Errors.DeepAll())
+                {
+                    errorMessages += "Error: " + (int)error.Code + " - " + error.Message + "\n";
+                }
+                TempData["Flash"] = errorMessages;
+                return RedirectToAction("New");
+            }
         }
     }
 }
--- BraintreeASPExample/Controllers/ShowController.cs
+++ BraintreeASPExample/Controllers/ShowController.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 
 namespace BraintreeASPExample.Controllers
 {
-    public class DropInController : Controller
+    public class ShowController : Controller
     {
         public IBraintreeConfiguration config = new BraintreeConfiguration();
 
@@ -20,63 +20,6 @@ namespace BraintreeASPExample.Controllers
                                                                                     TransactionStatus.SETTLEMENT_PENDING,
                                                                                     TransactionStatus.SUBMITTED_FOR_SETTLEMENT
                                                                                 };
-
-        public ActionResult New()
-        {
-            var gateway = config.GetGateway();
-            var clientToken = gateway.ClientToken.Generate();
-            ViewBag.ClientToken = clientToken;
-            return View();
-        }
-
-        public ActionResult Create()
-        {
-            var gateway = config.GetGateway();
-            System.Decimal amount;
-
-            try
-            {
-                amount = Convert.ToDecimal(Request["amount"]);
-            }
-            catch (FormatException e)
-            {
-                TempData["Flash"] = "Error: 81503: Amount is an invalid format.";
-                return RedirectToAction("New");
-            }
-
-            var nonce = Request["payment_method_nonce"];
-            var request = new TransactionRequest
-            {
-                Amount = amount,
-                PaymentMethodNonce = nonce,
-                Options = new TransactionOptionsRequest
-                {
-                    SubmitForSettlement = true
-                }
-            };
-
-            var result = gateway.Transaction.Sale(request);
-            if (result.IsSuccess())
-            {
-                Transaction transaction = result.Target;
-                return RedirectToAction("Show", new { id = transaction.Id });
-            }
-            else if (result.Transaction != null)
-            {
-                return RedirectToAction("Show", new { id = result.Transaction.Id } );
-            }
-            else
-            {
-                string errorMessages = "";
-                foreach (ValidationError error in result.Errors.DeepAll())
-                {
-                    errorMessages += "Error: " + (int)error.Code + " - " + error.Message + "\n";
-                }
-                TempData["Flash"] = errorMessages;
-                return RedirectToAction("New");
-            }
-
-        }
 
         public ActionResult Show(String id)
         {
